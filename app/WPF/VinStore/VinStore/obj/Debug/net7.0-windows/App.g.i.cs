da8ff85c8393adb5f426044c950f452fb64a9f8e
Aicha SELLAMI <aicha.sellami@viacesi.fr>
2024-01-08 14:22:19 +0100

    #hot-fix-app-push-debug

--- app/WPF/VinStore/VinStore/obj/Debug/net7.0-windows/App.g.i.cs
+++ app/WPF/VinStore/VinStore/obj/Debug/net7.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace VinStore {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/VinStore;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VinStore;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- app/WPF/VinStore/VinStore/obj/Debug/net7.0-windows/VinStore.AssemblyInfo.cs
+++ app/WPF/VinStore/VinStore/obj/Debug/net7.0-windows/VinStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VinStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28788361ff00078cd89f5d5ee7bcef1cc3fed97a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VinStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VinStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
