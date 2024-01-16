b03ae3fc6b1875e1df0075dab3cf14250403d930
vincouille <condellovince2004@gmail.com>
2024-01-16 16:36:10 +0100

    Changement du front de la fiche produit

--- website/src/app/product/page.tsx
+++ website/src/app/product/page.tsx
@@ -7,47 +7,43 @@ const Product = () => {
     return (
         <div>
             <Header/>
-            <div className="bg-gray-100 dark:bg-gray-800 py-8">
+            <div className="bg-white py-8">
                 <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex flex-col md:flex-row -mx-4">
                         <div className="md:flex-1 px-4">
-                            <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+                            <div className="h-[460px] rounded-lg bg-gray-300 mb-4">
                                 <img src={vinImage.src} alt="Product" className="w-full h-full object-cover"/>
                             </div>
                             <div className="flex -mx-2 mb-4">
                                 <div className="w-1/2 px-2">
-                                    <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Add to Cart</button>
+                                    <button className="w-full bg-gray-900 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800">Add to Cart</button>
                                 </div>
                                 <div className="w-1/2 px-2">
-                                    <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Add to Wishlist</button>
+                                    <button className="w-full bg-gray-200 text-gray-800 px-4 rounded-full font-bold hover:bg-gray-300">Add to Wishlist</button>
                                 </div>
                             </div>
-                        </div>
+                            </div>
                         <div className="md:flex-1 px-4">
-                            <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">Table Product.Name</h2>
-                            <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                                Table Product.Description
+                            <h2 className="text-2xl font-bold text-black mb-2">product.name</h2>
+                            <p className="text-black text-sm mb-4">
+                                product.description
                             </p>
                             <div className="flex mb-4">
                                 <div className="mr-4">
-                                    <span className="font-bold text-gray-700 dark:text-gray-300">Price:</span>
-                                    <span className="text-gray-600 dark:text-gray-300">Table Product.Price</span>
-                                </div>
-                                <div>
-                                    <span className="font-bold text-gray-700 dark:text-gray-300">Availability:</span>
-                                    <span className="text-gray-600 dark:text-gray-300">Table Stock.Quantity si Quantity suppérieur à 0</span>
+                                    <span className="font-bold text-black">Price:</span>
+                                    <span className="text-black">product.price</span>
                                 </div>
                             </div>
                             <div className="mb-4">
-                                <span className="font-bold text-gray-700 dark:text-gray-300">Select Size:</span>
+                                <span className="font-bold text-black">Select Size:</span>
                                 <div className="flex items-center mt-2">
-                                    <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">À l'unité</button>
-                                    <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">Pack 6</button>
+                                    <button className="bg-gray-300 text-gray-700 py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">À l'unité</button>
+                                    <button className="bg-gray-300 text-gray-700 py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">Pack 6</button>
                                 </div>
                             </div>
                             <div>
-                                <span className="font-bold text-gray-700 dark:text-gray-300">Product Description:</span>
-                                <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                                <span className="font-bold text-black">Product Description:</span>
+                                <p className="text-black text-sm mt-2">
                                     Table Product.Description
                                 </p>
                             </div>
