aadbba10f2e7346047ad90cb97ddc1b58d1460ab
vincouille <condellovince2004@gmail.com>
2024-01-24 18:07:52 +0100

    header et accueil modifiés au format tel

--- website/modules/layout/header.tsx
+++ website/modules/layout/header.tsx
@@ -20,31 +20,22 @@ const Header = () => {
   };
 
   return (
-    <nav className="border-gray-200 bg-transparent text-white stroke-white hover:bg-white hover:text-black hover:stroke-black transition-all duration-500 ease-in-out">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="bg-transparent text-white stroke-white hover:bg-white hover:text-black hover:stroke-black transition-all duration-500 ease-in-out">
+      <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4 h-16">
         <a href={'/'} className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo.src} className="h-8" alt="NegoSud Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap">NegoSud</span>
         </a>
-        <button
-          onClick={handleMenuToggle}
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 text-gray-400 focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={isMenuOpen ? 'true' : 'false'}
-        >
-         <Burger/>
-        </button>
-        <div className={`w-full md:block md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
+        <div className="block w-auto" id="navbar-default">
+          <ul className="font-medium flex mt-4 rounded-lg flex-row space-x-8 rtl:space-x-reverse">
             <li>
-              <a href={`${connected ? '/account' : '/login'}`} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-500 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
+              <a href={`${connected ? '/account' : '/login'}`} className="block py-2 text-gray-900 rounded hover:bg-gray-500 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
                 <User className="h-8 w-8"/>
               </a>
             </li>
             {connected ? (
             <li>
-              <a href={"/cart"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-500 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
+              <a href={"/cart"} className="block py-2 text-gray-900 rounded hover:bg-gray-500 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
                 <Cart className="h-8 w-8"/>
               </a>
             </li>)
