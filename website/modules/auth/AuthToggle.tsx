46dda33fb95a3b1055f6a47aff6d3f4643946283
adrien <adrien@carjager.com>
2024-02-14 10:51:46 +0100

    add credit card + improve login

--- website/modules/auth/AuthToggle.tsx
+++ website/modules/auth/AuthToggle.tsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 interface AuthToggleProps {
   onToggle: () => void;
   isLogin: boolean;
+  text: string;
+  className?: string;
 }
 
-const AuthToggle: React.FC<AuthToggleProps> = ({ onToggle }) => {
+const AuthToggle: React.FC<AuthToggleProps> = ({ onToggle, text, className }) => {
   const [isClicked, setIsClicked] = useState(false);
 
   const handleClick = () => {
@@ -14,25 +16,9 @@ const AuthToggle: React.FC<AuthToggleProps> = ({ onToggle }) => {
   };
 
   return (
-    <div
-      onClick={handleClick}
-      className="border w-fit rounded-xl m-5 shadow-sm"
-    >
-      <button
-        className={`px-4 py-2 rounded-l-xl  ${
-          isClicked ? 'bg-white text-black' : 'bg-red-500 text-white'
-        } transition-colors duration-300`}
-      >
-        Connexion
-      </button>
-      <button
-        className={`px-4 py-2 rounded-r-xl ${
-          isClicked ? 'bg-red-500 text-white' : 'bg-white text-black'
-        } transition-colors duration-300`}
-      >
-        Inscription
-      </button>
-    </div>
+    <span className={className} onClick={handleClick}>
+      {text}
+    </span>
   );
 };
 
